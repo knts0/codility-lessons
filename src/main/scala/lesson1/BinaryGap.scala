239446a5e1a0dc5ce1fccbd6f236d371fcf39a40
Kana Otawara <kana@Kana-no-MacBook-Air.local>
2019-07-08 00:40:56 +0900

    improve code (lesson 1)

--- src/main/scala/lesson1/BinaryGap.scala
+++ src/main/scala/lesson1/BinaryGap.scala
@@ -23,4 +23,25 @@ object BinaryGap {
 
     res
   }
+
+  // 最初のコードは汚かったので直しました。。
+  // 二進数表示の文字列は1... というように必ず1から始まるので
+  // 0の場合はcntを増やす、1の場合はcntとansのうち大きい方をとる
+  // としていけばansに答えが入る
+  // result: https://app.codility.com/demo/results/training87UHY9-6VA/
+  def solution2(n: Int): Int = {
+    val binaryString = Integer.toBinaryString(n).toSeq
+
+    var ans = 0
+    var cnt = 0
+    binaryString.foreach { v =>
+      if (v == '1') {
+        ans = math.max(ans, cnt)
+        cnt = 0
+      } else {
+        cnt += 1
+      }
+    }
+    ans
+  }
 }
