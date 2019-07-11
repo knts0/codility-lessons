29cf68b2ba7a6c20419abdbd6ac8b9048cbb15a8
Kana Otawara <kana@Kana-no-MacBook-Air.local>
2019-07-12 00:54:01 +0900

    lesson4の別解

--- src/main/scala/lesson4/FrogRiverOne.scala
+++ src/main/scala/lesson4/FrogRiverOne.scala
@@ -1,6 +1,7 @@
 package lesson4
 
 class FrogRiverOne {
+  // 各1〜xの位置には最短何秒後に葉が落ちるのかを求め、それらの最大値が答えとなる
   def solution(x: Int, a: Array[Int]): Int = {
     var times = Array.fill(100010)(-1)
 
@@ -14,5 +15,19 @@ class FrogRiverOne {
     }
     if (check) times.slice(1, x + 1).max
     else -1
+  }
+
+  // ↑のコードを簡潔にしようと頑張った
+  // result: https://app.codility.com/demo/results/trainingYQ3X2V-E66/
+  def solution2(x: Int, a: Array[Int]): Int = {
+    // 各1〜xの位置に、最短何秒後に葉が落ちるのかを格納した配列 (0-indexed)
+    val earliestTimes = a.zipWithIndex.foldLeft(Array.fill(x)(-1)) { case (acc, (v, idx)) =>
+      if (v <= x && acc(v - 1) == -1) acc(v - 1) = idx
+      acc
+    }
+    earliestTimes.foldLeft(-1) { (acc, v) =>
+      if (v == -1) return -1
+      math.max(acc, v)
+    }
   }
 }
--- src/main/scala/lesson4/PermCheck.scala
+++ src/main/scala/lesson4/PermCheck.scala
@@ -1,6 +1,8 @@
 package lesson4
 
 object PermCheck {
+  // HashMapに各数が何回登場したかをカウントしていって、
+  // 登場回数が1回以外の場合や、Nより大きい数が登場していた場合0を返すようにする
   def solution(a: Array[Int]): Int = {
     var cnt = collection.mutable.HashMap.empty[Int, Int]
 
@@ -14,5 +16,21 @@ object PermCheck {
       if (v._1 > a.length || v._2 != 1) res = 0
     }
     res
+  }
+
+  // 配列aの数をHashSetに格納していく。もしすでにHashSetに存在するならば、配列aには同じ数が2つ以上含まれることになる。
+  // 配列aに同じ数は含まれず、かつ、配列aの最大値がNであればpermutationである。
+  // がんばってfoldLeftで書いてみた
+  // result: https://app.codility.com/demo/results/trainingNJ2F2Q-KBE/
+  def solution2(a: Array[Int]): Int = {
+    var st = collection.mutable.HashSet.empty[Int]
+
+    val (isUnique, max) = a.foldLeft((true, 0)) { case ((isUnique, max), v) =>
+      val nextUnique = !st.contains(v)
+      st += v
+      (isUnique && nextUnique, math.max(max, v))
+    }
+    if (isUnique && max == a.length) 1
+    else 0
   }
 }
